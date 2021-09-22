51ab31816e1695b1720592feae1e7ff463f2bbb6
Titouan DENYS <titouan.denys@gmail.com>
2021-09-22 16:59:51 +0200

    Init

--- src/Controller/CourseController.php
+++ src/Controller/CourseController.php
@@ -2,7 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Item;
+use App\Form\ItemType;
+use App\Repository\ItemRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,10 +16,45 @@ class CourseController extends AbstractController
     /**
      * @Route("/course", name="course")
      */
-    public function index(): Response
+    public function index(ItemRepository $itemRepository, Request $request): Response
     {
+        $item = new Item();
+        $formItem = $this->createForm(ItemType::class,$item);
+        $formItem->handleRequest($request);
+        if ( $formItem->isSubmitted()){
+            $item->setIsBuy(false);
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($item);
+            $em->flush();
+            return $this->redirectToRoute('course');
+        }
+
         return $this->render('course/index.html.twig', [
             'controller_name' => 'CourseController',
+            'items' => $itemRepository->findAll(),
+            'formItem'=> $formItem->createView()
         ]);
+    }
+
+    /**
+     * @Route("/acheter/{id}", name="acheter")
+     */
+    public function acheter(Item $item): Response
+    {
+        $em = $this->getDoctrine()->getManager();
+        $item->setIsBuy(!$item->getIsBuy());
+        $em->flush();
+        return $this->redirectToRoute('course');
+    }
+
+    /**
+     * @Route("/supprimer/{id}", name="supprimer")
+     */
+    public function supprimer(Item $item, EntityManagerInterface $em): Response
+    {
+        $em = $this->getDoctrine()->getManager();
+        $em->remove($item);
+        $em->flush();
+        return $this->redirectToRoute('course');
     }
 }
---
+++ src/Entity/Item.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ItemRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ItemRepository::class)
+ */
+class Item
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $libelle;
+
+    /**
+     * @ORM\Column(type="boolean")
+     */
+    private $isBuy;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLibelle(): ?string
+    {
+        return $this->libelle;
+    }
+
+    public function setLibelle(string $libelle): self
+    {
+        $this->libelle = $libelle;
+
+        return $this;
+    }
+
+    public function getIsBuy(): ?bool
+    {
+        return $this->isBuy;
+    }
+
+    public function setIsBuy(bool $isBuy): self
+    {
+        $this->isBuy = $isBuy;
+
+        return $this;
+    }
+}
